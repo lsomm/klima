7a1bbd53df45e3008a3ac2913124ec61d114a9c1
lsomm <luca.l.sommer@protonmail.com>
2023-06-22 10:03:14 +0200

    removed axis

--- 3dplot.jl
+++ 3dplot.jl
@@ -2,6 +2,7 @@ using PlotlyJS
 using DelimitedFiles
 
 
+
 function plot_sphere(surface_data, radius)
 
     lat_resolution, long_resolution = size(surface_data)
@@ -16,10 +17,25 @@ function plot_sphere(surface_data, radius)
     Y = radius*sin.(lat_grid).*sin.(long_grid)
     Z = radius*cos.(lat_grid)
 
-    trace = surface(x=X, y=Y, z=Z, surfacecolor=geo')
 
-    plot(trace)
-end
+    fig = PlotlyJS.surface(x=X, y=Y, z=Z, surfacecolor=geo')
+    
+    layout = Layout(
+                scene=attr(
+                    xaxis=attr(
+                        visible=false
+                    ),
+                    yaxis=attr(
+                        visible=false
+                    ),
+                    zaxis=attr(
+                        visible=false
+                    )
+                )
+            )
+    
+    plot(fig, layout)   
+    end
 
 geo = readdlm("input/The_World128x65.dat")
 plot_sphere(geo, 10)
