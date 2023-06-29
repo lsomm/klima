613ca7b9f2abe0c3884ada59919b38b7c7ae5689
lsomm <luca.l.sommer@protonmail.com>
2023-06-29 13:17:23 +0200

    improved formatting

--- 3dplot2.jl
+++ 3dplot2.jl
@@ -8,8 +8,9 @@ using DelimitedFiles
 # In Klimakoffer einbauen / Temperatur/etc plots damit machen
 # Grid anzeigen?
 # Welt höher aufgelöst
+# Dokumentation
 
-function plot_sphere(surface_data, radius)
+function plot_earth(surface_data, radius)
 
     lat_resolution, long_resolution = size(surface_data)
 
@@ -24,33 +25,58 @@ function plot_sphere(surface_data, radius)
     Z = radius*cos.(lat_grid)
 
 
-    fig1 = PlotlyJS.surface(x=X, y=Y, z=Z, surfacecolor=geo', colorscale=
-        [
+    fig1 = PlotlyJS.surface(
+        x = X,
+        y = Y,
+        z = Z,
+        surfacecolor = geo',
+        colorscale = [
             [0, "rgb(60,149,33)"], #land, green
             [0.25, "rgb(165,255,255)"], #sea ice, icy blue
             [0.5,"rgb(255,255,255)"], #snow, white
             [1,"rgb(26,100,212)" ], #ocean, blue
         ],
-        colorbar = (autotick= false,  tick0= 0, dtick= 1, tickcolor=888,tickvals= [1,2,3,4,5], ticktext=["land","sea ice","snow cover","lakes","ocean"])
+        colorbar = (
+            autotick = false, 
+            tick0 = 0,
+            dtick = 1,
+            tickcolor = 888,
+            tickvals = [1,2,3,4,5],
+            ticktext = [
+                "land",
+                "sea ice",
+                "snow cover",
+                "lakes",
+                "ocean"
+            ]
+        )
     )
     
-    fig2 = PlotlyJS.surface(x=1.01.*X, y=1.01.*Y, z=1.01.*Z,contours = attr(geo=attr(show=true, size=0.05)), showscale=false, opacity=0.0,
+    fig2 = PlotlyJS.surface(
+        x = 1.01 .* X,
+        y = 1.01 .* Y,
+        z = 1.01 .* Z,
+        contours = attr(
+            geo = attr(
+                show = true,
+                size = 0.05
+            )
+        ),
+        showscale = false,
+        opacity = 0.0,
     )
     layout = Layout(
-                scene=attr(
-                    xaxis=attr(
-                        visible=false
+                scene = attr(
+                    xaxis = attr(
+                        visible = false
                     ),
-                    yaxis=attr(
-                        visible=false
+                    yaxis = attr(
+                        visible = false
                     ),
-                    zaxis=attr(
-                        visible=false
+                    zaxis = attr(
+                        visible = false
                     ),
-
-     
                 ),
-  
             )
 
 
@@ -58,4 +84,4 @@ function plot_sphere(surface_data, radius)
     end
 
 geo = readdlm("input/The_World128x65.dat")
-plot_sphere(geo, 10)
+plot_earth(geo, 10)
