bb3f4cf45fdbd6859a7212a7147c68fe386f23b9
lsomm <luca.l.sommer@protonmail.com>
2023-07-03 17:08:16 +0200

    update chong son

--- 3dplot2.jl
+++ 3dplot2.jl
@@ -12,7 +12,7 @@ include("milestone2.jl")
 # Himmel/Sterne/Weltall anzeigen
 # Solar mechanics ?
 
-function plot_earth(surface_data, radius)
+function parametrisierung(surface_data, radius=10)
 
     lat_resolution, long_resolution = size(surface_data)
 
@@ -26,12 +26,30 @@ function plot_earth(surface_data, radius)
     Y = radius*sin.(lat_grid).*sin.(long_grid)
     Z = radius*cos.(lat_grid)
 
+    return X,Y,Z
+
+end
+
+function plot_earth(X,Y,Z,surface_data)
+    #=
+    lat_resolution, long_resolution = size(surface_data)
+
+    latitude = range(0, pi, lat_resolution)
+    longitude = range(0, 2*pi, long_resolution)
+
+    lat_grid = latitude'.*ones(long_resolution)
+    long_grid = ones(lat_resolution)'.*longitude
+
+    X = radius*sin.(lat_grid).*cos.(long_grid)
+    Y = radius*sin.(lat_grid).*sin.(long_grid)
+    Z = radius*cos.(lat_grid)
+    =#
 
     fig1 = PlotlyJS.surface(
         x = X,
         y = Y,
         z = Z,
-        surfacecolor = geo',
+        surfacecolor = surface_data',
         colorscale = [
             [0, "rgb(60,149,33)"], #land, green
             [0.25, "rgb(165,255,255)"], #sea ice, icy blue
@@ -90,8 +108,9 @@ function plot_earth(surface_data, radius)
     PlotlyJS.plot([fig1,fig2], layout)   
 end
 
-function plot_data_field(data_field, surface_data, radius=10)
-
+function plot_data_field(X,Y,Z, data_field, surface_data)
+    # function plot_data_field(data_field, surface_data, radius=10)
+    #=
     lat_resolution, long_resolution = size(surface_data)
 
     latitude = range(0, pi, lat_resolution)
@@ -103,7 +122,7 @@ function plot_data_field(data_field, surface_data, radius=10)
     X = radius*sin.(lat_grid).*cos.(long_grid)
     Y = radius*sin.(lat_grid).*sin.(long_grid)
     Z = radius*cos.(lat_grid)
-
+    =#
 
     fig1 = PlotlyJS.surface(
         x = X,
@@ -160,8 +179,10 @@ function get_outlines(geo_dat)
     return round.(Int, outlines)
 end
 
-function plot_albedo_3d(albedo, surface_data, radius=10)
+function plot_albedo_3d(X,Y,Z, albedo, surface_data)
+    # function plot_albedo_3d(albedo, surface_data, radius=10)
 
+    #=
     lat_resolution, long_resolution = size(surface_data)
 
     latitude = range(0, pi, lat_resolution)
@@ -173,7 +194,7 @@ function plot_albedo_3d(albedo, surface_data, radius=10)
     X = radius*sin.(lat_grid).*cos.(long_grid)
     Y = radius*sin.(lat_grid).*sin.(long_grid)
     Z = radius*cos.(lat_grid)
-
+    =#
 
     fig1 = PlotlyJS.surface(
         x = X,
@@ -197,7 +218,105 @@ function plot_albedo_3d(albedo, surface_data, radius=10)
         colorscale = [
             [0, "rgb(0,0,0)"], # black, nothing reflected
             [1,"rgb(255,255,255)" ], # white, everything reflected
-        ]
+        ],
+        colorbar = (
+            autotick = false, 
+            tickcolor = 888,
+            tickfont = (
+                color = "rgb(255,255,255)",
+                size = 15
+                ),
+            title="albedo",
+            titlefont = (
+                color = "rgb(255,255,255)",
+                size = 15
+                )   
+        )
+    )
+    
+    layout = Layout(
+                scene = attr(
+                    xaxis = attr(
+                        visible = false
+                    ),
+                    yaxis = attr(
+                        visible = false
+                    ),
+                    zaxis = attr(
+                        visible = false
+                    ),
+                ),
+                paper_bgcolor = "black"
+            )
+
+
+    PlotlyJS.plot([fig1,fig2], layout)   
+
+end
+
+function plot_heatcapacity_3d(X,Y,Z, heat_capacity, surface_data)
+    # function plot_heatcapacity_3d(heat_capacity, surface_data, radius=10)
+
+    #=
+    lat_resolution, long_resolution = size(surface_data)
+
+    latitude = range(0, pi, lat_resolution)
+    longitude = range(0, 2*pi, long_resolution)
+
+    lat_grid = latitude'.*ones(long_resolution)
+    long_grid = ones(lat_resolution)'.*longitude
+
+    X = radius*sin.(lat_grid).*cos.(long_grid)
+    Y = radius*sin.(lat_grid).*sin.(long_grid)
+    Z = radius*cos.(lat_grid)
+    =#
+
+    heat_capacity_log = log10.(heat_capacity)
+
+    vmin = minimum(heat_capacity_log)
+    vmax = maximum(heat_capacity_log)
+
+    cbar_ticks = LinRange(vmin, vmax, 10)
+    cbar_tick_values = [@sprintf("%.3f", 10^tick) for tick in cbar_ticks]
+
+    fig1 = PlotlyJS.surface(
+        x = X,
+        y = Y,
+        z = Z,
+        surfacecolor = surface_data',
+        showscale = false,
+        colorscale = [
+            [0, "rgb(255,255,255)"], # no border
+            [1,"rgb(0,0,0)" ], # continent border
+        ],
+    )
+    
+    fig2 = PlotlyJS.surface(
+        x = 1.01 .* X,
+        y = 1.01 .* Y,
+        z = 1.01 .* Z,
+        surfacecolor = heat_capacity_log',
+        showscale = true,
+        opacity = 0.8,
+        colorscale =[
+            [0, "rgb(255,255,255)" ], # white
+            [1, "rgb(139,0,0)"], # red 
+        ],
+        colorbar = (
+            autotick = false, 
+            tickcolor = 888,
+            tickfont = (
+                color = "rgb(255,255,255)",
+                size = 15
+                ),
+            tickvals = cbar_ticks,
+            ticktext = cbar_tick_values,
+            title="heat capacity",
+            titlefont = (
+                color = "rgb(255,255,255)",
+                size = 15
+                )   
+        )
     )
     
     layout = Layout(
@@ -223,6 +342,13 @@ end
 geo = readdlm("input/The_World128x65.dat")
 
 albedo = calc_albedo(geo)
+heat_capacity = calc_heat_capacity(geo)
 
 #plot_data_field(fake_temps, get_outlines(geo),10)
-plot_albedo_3d(albedo, get_outlines(geo))
+
+# Parametrisierung
+X,Y,Z = parametrisierung(geo)
+display(plot_earth(X,Y,Z,geo)) # earth
+display(plot_albedo_3d(X,Y,Z, albedo, get_outlines(geo))) # albedo
+plot_heatcapacity_3d(X,Y,Z, heat_capacity, get_outlines(geo)) # heat capacity
+
